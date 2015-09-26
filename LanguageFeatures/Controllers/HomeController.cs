707a8a0348873c0cb9b43ee098bca6a4e02f9f5b
Peska <peska_jr@o2.pl>
2015-09-26 19:57:23 +0200

    Properties chapter

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+	public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Navigate to URL to show an example";
+        }
+
+		public ViewResult AutoProperty()
+		{
+			// create na new Product object
+			Product product = new Product();
+
+			// set the property value
+			product.Name = "Kayak";
+
+			// get the property
+			string productName = product.Name;
+
+			// generate the view
+			return View("Result", (object)string.Format("Product name: {0}", productName));
+		}
+    }
+}
